e4e21275fdcbc3ec2a43906ee0c4777ed827d5c2
Balram Rai <balrai@meb.ki.se>
2025-04-17 16:09:29 +0200

    change description and add fucntion for preprocessing the text

---
+++ R/preprocess_text.R
@@ -0,0 +1,17 @@
+#' Preprocess Text
+#'
+#' This function reads a text document, converts it to lowercase, removes punctuation, and splits it into words.
+#'
+#' @param file_path Path to the text file (TXT).
+#' @return A vector of cleaned and processed words.
+#' @examples
+#' words <- preprocess_text("example.txt")
+#' head(words)
+preprocess_text <- function(file_path) {
+  text_file <- read.delim(file_path, header = FALSE, stringsAsFactors = FALSE)
+  text_lower <- tolower(text_file$V1)
+  text_clean <- gsub("[[:punct:]]", "", text_lower)
+  word_vector <- unlist(strsplit(text_clean, split = " "))
+  word_vector <- word_vector[word_vector != ""]
+  return(word_vector)
+}
