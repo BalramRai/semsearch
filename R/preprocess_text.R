b8fff059ed21a84698842cc6c544367a9a77ef35
BalramRai <balram.rai@ki.se>
2025-04-22 14:19:54 +0200

    Included the comments for the preprocess_text function

--- R/preprocess_text.R
+++ R/preprocess_text.R
@@ -8,10 +8,10 @@
 #' words <- preprocess_text("example.txt")
 #' head(words)
 preprocess_text <- function(file_path) {
-  text_file <- read.delim(file_path, header = FALSE, stringsAsFactors = FALSE, fill = TRUE)
-  text_lower <- tolower(text_file$V1)
-  text_clean <- gsub("[[:punct:]]", "", text_lower)
-  word_vector <- unlist(strsplit(text_clean, split = " "))
-  word_vector <- word_vector[word_vector != ""]
+  text_file <- read.delim(file_path, header = FALSE, stringsAsFactors = FALSE, fill = TRUE) # read a text file and handles unequal rows
+  text_lower <- tolower(text_file$V1) # convert all the text to lowercase letters
+  text_clean <- gsub("[[:punct:]]", "", text_lower) # remove punctuations using POXIS character class [:punct:] including ',',':',';', etc
+  word_vector <- unlist(strsplit(text_clean, split = " ")) # split the string into a vector of words using single space delimiter
+  word_vector <- word_vector[word_vector != ""] # removes the empty strings if any created
   return(word_vector)
 }
